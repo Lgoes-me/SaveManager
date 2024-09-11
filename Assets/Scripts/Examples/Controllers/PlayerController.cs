c19e5cf170bb401dae5336c3dbf601004ae190e2
Góes <lucas.rodrigues.goes@gmail.com>
2024-09-11 00:52:32 -0300

    Simplificando

--- Assets/Scripts/Examples/Controllers/PlayerController.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class PlayerController : MonoBehaviour
-{
-    [field: SerializeField] public string Name { get; set; }
-    [field: SerializeField] public int Score { get; set; }
-    
-}
